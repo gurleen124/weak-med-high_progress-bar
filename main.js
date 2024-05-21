7b03d320c934e39a58e54e30ddf09f8eb9d6a17f
gurleen124 <gurleen.kaur@lpinfotech.com>
2024-05-21 15:44:23 +0530

    complete

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-// let progressBar = document.getElementById("progress-bar");
-// let width = parseInt(progressBar.style.width);
-// progressBar.addEventListener('click', function(){
-//     width += 10;
-//     progressBar.style.width = width + '%';
-// })
+let progressBar = document.getElementById("progress-bar");
+let inputWidth = document.getElementById("inputWidth");
+let submit = document.getElementById('submitbtn');
+
+// increasing the width on submit button click
+submit.addEventListener('click', function () {
+    let input = inputWidth.value;
+    progressBar.style.width = input + "%";
+})
+
+
