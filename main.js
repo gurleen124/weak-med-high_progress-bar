b225b9eab299ac2ec66ae3e3dc9f2cd5fb17c37a
gurleen124 <gurleen.kaur@lpinfotech.com>
2024-05-21 11:43:32 +0530

    first

---
+++ main.js
@@ -0,0 +1,6 @@
+// let progressBar = document.getElementById("progress-bar");
+// let width = parseInt(progressBar.style.width);
+// progressBar.addEventListener('click', function(){
+//     width += 10;
+//     progressBar.style.width = width + '%';
+// })
